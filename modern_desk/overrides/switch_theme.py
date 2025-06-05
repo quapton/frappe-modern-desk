6077d8fb035871be7e6ae45bfa82b555c26f33bb
Quapton Developers <developers@quapton.com>
2025-06-05 18:14:59 +0530

    Update switch_theme.py

--- modern_desk/overrides/switch_theme.py
+++ modern_desk/overrides/switch_theme.py
@@ -2,5 +2,5 @@ import frappe
 
 @frappe.whitelist()
 def switch_theme(theme):
-	if theme in ["Dark", "Light", "Automatic", "Modern_ui_theme"]:
+	if theme in ["Dark", "Light", "Automatic", "Quapton AI"]:
 		frappe.db.set_value("User", frappe.session.user, "desk_theme", theme)
